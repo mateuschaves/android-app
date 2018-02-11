be3a21d052eb044d949f795658c40eb1a159424a
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-02-11 14:12:53 -0300

    Storing complex data

--- app/src/main/java/com/example/mateus/curso/MainActivity.java
+++ app/src/main/java/com/example/mateus/curso/MainActivity.java
@@ -9,26 +9,32 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button mFirebaseBtn;
-    private EditText name;
-
+    private EditText mName;
+    private EditText mEmail;
     private DatabaseReference mDatabse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mFirebaseBtn = (Button) findViewById(R.id.firebase_btn);
-        name = (EditText) findViewById(R.id.name);
-        mDatabse = FirebaseDatabase.getInstance().getReference();
-
+        mFirebaseBtn    =   (Button) findViewById(R.id.firebase_btn);
+        mName           =   (EditText) findViewById(R.id.name);
+        mEmail          =   (EditText) findViewById(R.id.email);
+        mDatabse        =    FirebaseDatabase.getInstance().getReference();
         mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                    mDatabse.push().setValue(name.getText().toString());
+                String name = mName.getText().toString().trim();
+                String email = mEmail.getText().toString().trim();
+                HashMap<String, String> dataMap = new HashMap<String, String>();
+                dataMap.put("Name", name);
+                dataMap.put("Email", email);
+                mDatabse.push().setValue(dataMap);
             }
         });
     }
