55a25a6f6f809f3bf4088c79861bae542e2f9b0c
Mateus Henrique <mateus.henriquechaves@ufpe.br>
2018-02-11 14:18:18 -0300

    Toast added

--- app/src/main/java/com/example/mateus/curso/MainActivity.java
+++ app/src/main/java/com/example/mateus/curso/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.mateus.curso;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -34,7 +38,16 @@ public class MainActivity extends AppCompatActivity {
                 HashMap<String, String> dataMap = new HashMap<String, String>();
                 dataMap.put("Name", name);
                 dataMap.put("Email", email);
-                mDatabse.push().setValue(dataMap);
+                mDatabse.push().setValue(dataMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(MainActivity.this, "Stored", Toast.LENGTH_LONG).show();
+                        }else{
+                            Toast.makeText(MainActivity.this, "Error... ", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
             }
         });
     }
